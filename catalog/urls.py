9e632835b1a6d38d7b7b58e47a4235cd122b352e
ttt.temaa <artem1005200776@gmail.com>
2024-12-11 15:43:23 +0300

    Реализация поставленной задачи 23 Работа с ORM в Django

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product_detail, products_list
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
-    path('', contacts, name='contacts')
+    path('', contacts, name='contacts'),
+    path("product_detail/<int:pk>", product_detail, name='product_detail'),
+    path('home', products_list, name='products_list')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
+from catalog.models import Product
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'catalog/home.html')
 
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    return render(request, 'catalog/contacts.html')
+
+
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {
+        "product_name": product.name,
+        "product_description": product.description,
+        "product_image": product.image,
+        "product_category": product.category,
+        "product_price": product.price,
+        "product_created_at": product.created_at,
+        "product_updated_at": product.updated_at
+    }
+    return render(request, "catalog/product_detail.html", context=context)
+
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'catalog/home.html', context)
