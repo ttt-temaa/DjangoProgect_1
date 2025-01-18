5c56d23df5101163fe71396a197338be94ea2efd
ttt.temaa <artem1005200776@gmail.com>
2025-01-18 17:57:45 +0300

    Реализация поставленной задачи урока 26.1 "Формы"

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+
+from catalog.views import ProductCreateView, ProductUpdateView
 from catalog.apps import CatalogConfig
 from catalog.views import HomeView, ContactsView, ProductDetailView, ProductListView
 
@@ -8,5 +10,7 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('', ContactsView.as_view(), name='contacts'),
     path("product_detail/<int:pk>", ProductDetailView.as_view(), name='product_detail'),
-    path('home', ProductListView.as_view(), name='product_list')
+    path('home', ProductListView.as_view(), name='product_list'),
+    path('products/create_product', ProductCreateView.as_view(), name='product_create'),
+    path('products/update_product/<int:pk>', ProductUpdateView.as_view(), name='product_update')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,24 @@
-from django.views.generic import ListView, DetailView, TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView
 
 from catalog.models import Product
+
+from catalog.forms import ProductForm
+from catalog.models import Product
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product_list')
 
 
 class HomeView(TemplateView):
